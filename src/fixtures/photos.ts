51c590ab964fbac54cc2471fd56f3178b726e7b0
Patrick Rotter <patrick.rotter@gmail.com>
2022-07-22 17:29:10 +0200

    Added folder structure and data

---
+++ src/fixtures/photos.ts
@@ -0,0 +1,49 @@
+import { Photo } from "../global/types";
+
+export const photos: Photo[] = [
+  {
+    key: 1,
+    author: "Johanna Hobel",
+    commissionedFor: "Vogue",
+    url: "./media/image01.jpg",
+    text: "Everyday Flowers",
+    date: new Date(2019, 6),
+    link: "link",
+  },
+  {
+    key: 2,
+    author: "Johanna Hobel",
+    commissionedFor: "wild",
+    url: "./media/image02.jpg",
+    text: "The wilder Night",
+    date: new Date(2019, 12),
+    link: "link",
+  },
+  {
+    key: 3,
+    author: "Johanna Hobel",
+    commissionedFor: "Chanel",
+    url: "./media/image03.jpg",
+    text: "Smooth Memories",
+    date: new Date(2020, 2),
+    link: "link",
+  },
+  {
+    key: 4,
+    author: "Johanna Hobel",
+    commissionedFor: "ON",
+    url: "./media/image04.jpg",
+    text: "The Future Universe",
+    date: new Date(2020, 4),
+    link: "link",
+  },
+  {
+    key: 5,
+    author: "Johanna Hobel",
+    commissionedFor: "SI",
+    url: "./media/image05.jpg",
+    text: "She Was Born Urban",
+    date: new Date(2021, 12),
+    link: "link",
+  },
+];
---
+++ src/global/types.ts
@@ -0,0 +1,9 @@
+export type Photo = {
+  key: number;
+  author: string;
+  commissionedFor: string;
+  url: string;
+  text: string;
+  date: Date;
+  link: string;
+};
